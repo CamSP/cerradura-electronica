ddca51b698efbef948e9c381a77e670ed196d1d4
Camilo Esteban Zambrano Pereira <53503084+CamSP@users.noreply.github.com>
2022-02-01 15:54:00 -0500

    Initial Commit

---
+++ cerradura/cerraduraApp.swift
@@ -0,0 +1,17 @@
+//
+//  cerraduraApp.swift
+//  cerradura
+//
+//  Created by Camilo Zambrano on 1/02/22.
+//
+
+import SwiftUI
+
+@main
+struct cerraduraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
